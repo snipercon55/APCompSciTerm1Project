02814355ac0ba149612d476d885d66a6d5f3b319
snipercon55 <snipercon55@gmail.com>
2021-11-02 17:21:06 -0500

    Add files via upload

---
+++ src/dice/diceMain.java
@@ -0,0 +1,14 @@
+package dice;
+
+public class diceMain {
+
+	public static void main(String[] args) {
+		dice myDice = new dice();
+		myDice.EnterRollNum();
+		myDice.DiceRoller();
+		myDice.rolls();
+		myDice.RepRolls();
+
+	}
+
+}
